ee2e47fa1bbf4907068c11c47455dc2180b2e1aa
Ars569F <86305814+Ars569F@users.noreply.github.com>
2022-12-29 10:24:03 +0200

    Adding files to the dedicated folder

---
+++ Java_project/src/Lesson2/Task1.java
@@ -0,0 +1,11 @@
+package Lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("\t\t\РУБАИ");
+        System.out.println("rerwrwrewdfssdf");
+        System.out.println("ewqeqweqweqweqwe");
+        System.out.println("sadasdasasdasdasd");
+
+    }
+}
---
+++ Java_project/src/Lesson2/Variables.java
@@ -0,0 +1,16 @@
+package Lesson2;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte;
+
+        myByte = 4;
+
+        byte myByte = 6;
+
+        int myInt = 12313123;
+
+        long myLong = 79879879879879L;
+
+    }
+}
