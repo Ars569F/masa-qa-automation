ee31b11bbc29e4910ee06643fe28c8e9ad9dcc8a
Ars569F <86305814+Ars569F@users.noreply.github.com>
2022-12-30 10:25:52 +0200

    adding gitignore

--- Java_project/src/Lesson2/Task1.java
+++ Java_project/src/Lesson2/Task1.java
@@ -2,7 +2,7 @@ package Lesson2;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("\t\t\РУБАИ");
+        System.out.println("РУБАИ");
         System.out.println("rerwrwrewdfssdf");
         System.out.println("ewqeqweqweqweqwe");
         System.out.println("sadasdasasdasdasd");
---
+++ Java_project/src/Lesson2_HomeTask/l2_HomeTask.java
@@ -0,0 +1,54 @@
+package Lesson2_HomeTask;
+
+public class l2_HomeTask {
+    public static void main(String[] args) {
+
+        byte myByte;
+        myByte = 2;
+
+        byte myByte2;
+        myByte2 = 4;
+
+        short myShort = 2313;
+        short myShort2 = 3141;
+
+        int myInt = 13213213;
+        int myInt2 = 321313;
+
+        long myLong = 43242525242422L;
+        long myLong2 = 3213123123123123L;
+        System.out.println(myByte);
+        System.out.println(myByte2);
+        System.out.println(myShort);
+        System.out.println(myShort2);
+        System.out.println(myInt);
+        System.out.println(myInt2);
+        System.out.println(myLong);
+        System.out.println(myLong2);
+
+
+        float myFloat = 4.45f;
+        double myDouble = 56.78;
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+
+        boolean istrue = true;
+        boolean isfalse = false;
+
+        System.out.println(istrue);
+        System.out.println(isfalse);
+
+        char myChar1 = 'F';
+        char myChar2 = 'p';
+
+        String myName = "Arse";
+
+
+        System.out.println(myChar1);
+        System.out.println(myChar2);
+        System.out.println("myName = " + myName);
+
+    }
+
+
+}
